604bd10cc9643294b6a07ba393e76d136fc0079b
Raktim Routh <raktim@logicasana.com>
2026-06-09 18:48:08 +0530

    react course uodated components

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,25 +1,47 @@
-function Header(){
-  return (
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-
-  );
+import componentsImg from './assets/components.png'
+import { CORE_CONCEPTS } from './data';
+import CoreConcept from './components/Coreconcepts';
+import Header from './components/Header';
+import TabButton from './components/TabButton';
+import { useState } from 'react';
+const reactDescriptions = ["Fundamental","Crucial","Core"]
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max+1));
 }
 
 
+
+
 function App() {
+  const [tabContent, setTabcontent] = useState("Please click a button") 
+  function handleSelect(selectedButton){
+    setTabcontent(selectedButton)
+  }
   return (
     <div>
-      <Header/>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
+      <Header />
+      <section id="core-concepts">
+        <h2>Core concepts</h2>
+        <ul>
+          {CORE_CONCEPTS.map(item => {
+            return (
+
+              <CoreConcept title={item.title} description={item.description} img={item.image}/> 
+
+            )
+          })}
+        </ul>
+      </section>
+      <section id="examples">
+        <h2>Examples</h2>
+        <menu>
+          <TabButton onSelect={() => handleSelect("components")}>Components </TabButton>
+          <TabButton onSelect={() => handleSelect("jsx")}>JSX </TabButton>
+          <TabButton onSelect={() => handleSelect("props")}>Props </TabButton>
+          <TabButton onSelect={() => handleSelect("state")}>State </TabButton>
+        </menu>
+        {tabContent}
+      </section>
     </div>
   );
 }
