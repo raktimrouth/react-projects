f78c3b911488ec393a1b922e6ccc8aa5e80d5eab
Raktim Routh <raktim@logicasana.com>
2026-06-22 13:09:06 +0530

    new prject and tailwind css added

--- 06-refs-portals-practice-starting-project/src/App.jsx
+++ 06-refs-portals-practice-starting-project/src/App.jsx
@@ -1,9 +1,12 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-      <main>
+      <main className="h-screen my-8 flex gap-8">
         <Sidebar />
+        <NoProjectSelected />
       </main>
   );
 }
--- 06-refs-portals-practice-starting-project/src/components/Sidebar.jsx
+++ 06-refs-portals-practice-starting-project/src/components/Sidebar.jsx
@@ -1,11 +1,11 @@
+import Button from "./Button";
+
 export default function Sidebar() {
     return(
-        <aside>
-            <h2>Your Projects</h2>
+        <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+            <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
             <div>
-                <button>
-                    + Add Project
-                </button>
+                <Button btnText='+ Add Project' />
             </div>
             <ul>
             </ul>
